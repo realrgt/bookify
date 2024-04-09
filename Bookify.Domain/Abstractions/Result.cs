8647027ade17f56d27aadf1429b8a9d908143237
realrgt <ergitorubildo@gmail.com>
2024-04-09 20:56:31 +0200

    introduce error result pattern

---
+++ Bookify.Domain/Abstractions/Result.cs
@@ -0,0 +1,51 @@
+namespace Bookify.Domain.Abstractions;
+
+public class Result
+{
+    protected internal Result(bool isSuccess, Error error)
+    {
+        if (isSuccess && error != Error.None)
+        {
+            throw new InvalidOperationException();
+        }
+
+        if (!isSuccess && error == Error.None)
+        {
+            throw new InvalidOperationException();
+        }
+
+        IsSuccess = isSuccess;
+        Error = error;
+    }
+
+    public bool IsSuccess { get; }
+    public bool IsFailure => !IsSuccess;
+    public Error Error { get; }
+    
+    public static Result Success() => new(true, Error.None);
+    
+    public static Result Failure(Error error) => new(false, error);
+
+    public static Result<TValue> Success<TValue>(TValue value) => new(value, true, Error.None);
+
+    public static Result<TValue> Failure<TValue>(Error error) => new(default, false, error);
+
+    public static Result<TValue> Create<TValue>(TValue? value) =>
+        value is not null ? Success(value) : Failure<TValue>(Error.NullValue);
+}
+
+public class Result<TValue> : Result
+{
+    private readonly TValue? _value;
+    protected internal Result(TValue? value, bool isSuccess, Error error)
+        : base(isSuccess, error)
+    {
+        _value = value;
+    }
+
+    public TValue Value => IsSuccess
+        ? _value!
+  	  	: throw new InvalidOperationException("The value of a failure result should not be accessed. Use IsSuccess to check if the operation was successful.");
+    
+    public static implicit operator Result<TValue>(TValue value) => Create(value);
+}
