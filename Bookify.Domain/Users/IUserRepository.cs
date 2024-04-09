08764b03368132fd9ccdb4aa9ac21db8c87b76ac
realrgt <ergitorubildo@gmail.com>
2024-04-09 17:22:20 +0200

    add repository contracts

---
+++ Bookify.Domain/Users/IUserRepository.cs
@@ -0,0 +1,7 @@
+namespace Bookify.Domain.Users;
+
+public interface IUserRepository
+{
+    Task<User?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
+    void Add(User user);
+}
