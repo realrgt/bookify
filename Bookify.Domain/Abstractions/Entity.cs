1d15fb9c4bed7e663082dbbf73fdfcedff8eb296
realrgt <ergitorubildo@gmail.com>
2024-04-09 12:37:20 +0200

    initial commit

---
+++ Bookify.Domain/Abstractions/Entity.cs
@@ -0,0 +1,10 @@
+namespace Bookify.Domain.Abstractions;
+
+public abstract class Entity
+{
+    protected Entity(Guid id)
+    {
+      Id = id;
+    }
+    public Guid Id { get; init; }
+}
---
+++ Bookify.Domain/Apartments/Apartment.cs
@@ -0,0 +1,24 @@
+﻿using Bookify.Domain.Abstractions;
+
+namespace Bookify.Domain.Apartments;
+
+public sealed class Apartment : Entity
+{
+    public Apartment(Guid id) : base(id)
+    {
+    }
+
+    public string Name { get; private set; }
+    public string Description { get; private set; }
+    public string Country { get; private set; }
+    public string State { get; private set; }
+    public string ZipCode { get; private set; }
+    public string City { get; private set; }
+    public string Street { get; private set; }
+    public decimal PriceAmount { get; private set; }
+    public string PriceCurrency { get; private set; }
+    public decimal CleaningFeeAmount { get; private set; }
+    public string CleaningFeeCurrency { get; private set; }
+    public List<Amenity> Amenities { get; private set; } = [];
+    public DateTime? LastBookedOnUtc { get; private set; }
+}
