6e90584ffb77af823e613f630a70213dc3ce46d4
realrgt <ergitorubildo@gmail.com>
2024-04-10 16:16:29 +0200

    add booking query along its handler

---
+++ Bookify.Application/Bookings/GetBooking/GetBookingQueryHandler.cs
@@ -0,0 +1,53 @@
+﻿using Bookify.Application.Abstractions.Data;
+using Bookify.Application.Abstractions.Messaging;
+using Bookify.Domain.Abstractions;
+using Dapper;
+
+namespace Bookify.Application.Bookings.GetBooking;
+
+internal sealed class GetBookingQueryHandler : IQueryHandler<GetBookingQuery, BookingResponse>
+{
+    private readonly ISqlConnectionFactory _sqlConnectionFactory;
+
+    public GetBookingQueryHandler(ISqlConnectionFactory sqlConnectionFactory)
+    {
+        _sqlConnectionFactory = sqlConnectionFactory;
+    }
+
+    public async Task<Result<BookingResponse>> Handle(GetBookingQuery request, CancellationToken cancellationToken)
+    {
+        using var connection = _sqlConnectionFactory.CreateConnection();
+
+        const string sql = """
+            SELECT
+                id AS Id,
+                apartment_id AS ApartmentId,
+                user_id AS UserId,
+                status AS Status,
+                price_for_period_amount As PriceAmount,
+                price_for_period_currency As PriceCurrency,
+                cleaning_fee_amount As CleaningFeeAmount,
+                cleaning_fee_currency As CleaningFeeCurrency,
+                amenities_up_charge_amount As AmenitiesUpChargeAmount,
+                amenities_up_charge_currency As AmenitiesUpChargeCurrency,
+                total_price_amount As TotalPriceAmount,
+                total_price_currency As TotalPriceCurrency,
+                duration_start AS DurationStart,
+                duration_end AS DurationEnd,
+                created_on_utc AS CreatedOnUtc,
+            FROM
+                bookings
+            WHERE
+                id = @BookingId
+            """;
+
+        var booking = await connection.QueryFirstOrDefaultAsync<BookingResponse>(
+            sql,
+            new
+            {
+                request.BookingId
+            });
+
+        return booking!;
+    }
+}
--- Bookify.Application/Bookings/ReserveBooking/ReserveBookingCommand.cs
+++ Bookify.Application/Bookings/ReserveBooking/ReserveBookingCommand.cs
@@ -2,7 +2,7 @@ using Bookify.Application.Abstractions.Messaging;
 
 namespace Bookify.Application.Bookings.ReserveBooking;
 
-public record ReserveBookingCommand(
+public sealed record ReserveBookingCommand(
     Guid ApartmentId,
     Guid UserId,
     DateOnly StartDate,
