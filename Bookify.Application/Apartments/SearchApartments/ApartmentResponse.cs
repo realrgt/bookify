42f4515945acbb95b797ab64c376765c15451234
realrgt <ergitorubildo@gmail.com>
2024-04-10 16:49:47 +0200

    add apartment search query along its handler

---
+++ Bookify.Application/Apartments/SearchApartments/ApartmentResponse.cs
@@ -0,0 +1,10 @@
+namespace Bookify.Application.Apartments.SearchApartments;
+
+public sealed class ApartmentResponse
+{
+    public Guid Id { get; init; }
+    public string Name { get; init; }
+    public string Description { get; init; }
+    public decimal Price { get; init; }
+    public AddressResponse Address { get; set; }
+}
