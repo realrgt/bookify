15e7df21070af69f72ccdc234f9fc81bdd87cdc8
realrgt <ergitorubildo@gmail.com>
2024-04-10 13:40:37 +0200

    define required CQRS messaging abstractions

---
+++ Bookify.Application/Abstractions/Messaging/ICommand.cs
@@ -0,0 +1,15 @@
+using Bookify.Domain.Abstractions;
+using MediatR;
+
+namespace Bookify.Application.Abstractions.Messaging;
+
+public interface ICommand : IRequest<Result>, IBaseCommand 
+{
+}
+
+public interface ICommand<TResponse> : IRequest<Result<TResponse>>, IBaseCommand
+{
+}
+public interface IBaseCommand
+{
+}
---
+++ Bookify.Application/Abstractions/Messaging/IQuery.cs
@@ -0,0 +1,8 @@
+using Bookify.Domain.Abstractions;
+using MediatR;
+
+namespace Bookify.Application.Abstractions.Messaging;
+
+public interface IQuery<TResponse> : IRequest<Result<TResponse>>
+{
+}
