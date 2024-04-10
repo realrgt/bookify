7aa89994a27ec5cd6615871bb38b257b702a104e
realrgt <ergitorubildo@gmail.com>
2024-04-10 17:46:59 +0200

    define validation rules for reserve booking command

--- Bookify.Application/DependencyInjection.cs
+++ Bookify.Application/DependencyInjection.cs
@@ -1,5 +1,6 @@
 ﻿using Bookify.Application.Abstractions.Behaviors;
 using Bookify.Domain.Bookings;
+using FluentValidation;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Bookify.Application;
@@ -14,6 +15,8 @@ public static class DependencyInjection
             configuration.AddOpenBehavior(typeof(LoggingBehavior<,>));
             configuration.AddOpenBehavior(typeof(ValidationBehavior<,>));
         });
+
+        services.AddValidatorsFromAssembly(typeof(DependencyInjection).Assembly);
 
         services.AddTransient<PricingService>();
 
