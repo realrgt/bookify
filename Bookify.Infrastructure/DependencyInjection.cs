a2ade2a31a7b15e6d8bb14b730c02d10b82bdf70
realrgt <ergitorubildo@gmail.com>
2024-04-11 15:58:13 +0200

    add infrastructure project to the solution

---
+++ Bookify.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,22 @@
+﻿using Bookify.Application.Abstractions.Clock;
+using Bookify.Application.Abstractions.Email;
+using Bookify.Infrastructure.Clock;
+using Bookify.Infrastructure.Email;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Bookify.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        services.AddTransient<IDateTimeProvider, DateTimeProvider>();
+        services.AddTransient<IEmailService, EmailService>();
+
+        return services;
+    }
+}
+
