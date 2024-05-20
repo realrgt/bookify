455169398b79ce1091aec975b4b4a38e46246087
realrgt <ergitorubildo@gmail.com>
2024-05-20 19:57:16 +0200

    configure authentication options

--- Bookify.Infrastructure/DependencyInjection.cs
+++ Bookify.Infrastructure/DependencyInjection.cs
@@ -5,6 +5,7 @@ using Bookify.Domain.Abstractions;
 using Bookify.Domain.Apartments;
 using Bookify.Domain.Bookings;
 using Bookify.Domain.Users;
+using Bookify.Infrastructure.Authentication;
 using Bookify.Infrastructure.Clock;
 using Bookify.Infrastructure.Data;
 using Bookify.Infrastructure.Email;
@@ -31,6 +32,8 @@ public static class DependencyInjection
         services
             .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
             .AddJwtBearer();
+        services.Configure<AuthenticationOptions>(configuration.GetSection("Authentication"));
+        services.ConfigureOptions<JwtBearerOptionsSetup>();
 
         return services;
     }
