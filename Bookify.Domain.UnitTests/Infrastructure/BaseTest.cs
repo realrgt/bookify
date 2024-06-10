f354c30f4936a0b28fe94b508a78abfb710c05d0
realrgt <ergitorubildo@gmail.com>
2024-06-10 16:33:06 +0200

    add base test module to assert domain events

---
+++ Bookify.Domain.UnitTests/Infrastructure/BaseTest.cs
@@ -0,0 +1,20 @@
+﻿using Bookify.Domain.Abstractions;
+
+namespace Bookify.Domain.UnitTests;
+
+public abstract class BaseTest
+{
+    public static T AssertDomainEventWasPublished<T>(Entity entity)
+    {
+        var domainEvent = entity.GetDomainEvents()
+            .OfType<T>()
+            .SingleOrDefault();
+
+        if (domainEvent == null)
+        {
+            throw new Exception($"{typeof(T).Name} was not published.");
+        }
+
+        return domainEvent;
+    }
+}
