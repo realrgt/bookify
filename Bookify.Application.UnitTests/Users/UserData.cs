eb4dcf28b4152c2f07683c0fc8ed19f3abefb678
realrgt <ergitorubildo@gmail.com>
2024-06-10 23:09:59 +0200

    add unit tests for the reserve booking command handler

---
+++ Bookify.Application.UnitTests/Users/UserData.cs
@@ -0,0 +1,12 @@
+﻿using Bookify.Domain.Users;
+
+namespace Bookify.Application.UnitTests;
+
+internal static class UserData
+{
+    public static User Create() => User.Create(FirstName, LastName, Email);
+
+    public static readonly FirstName FirstName = new("John");
+    public static readonly LastName LastName = new("Doe");
+    public static readonly Email Email = new("john.doe@test.com");
+}
