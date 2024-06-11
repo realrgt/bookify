6beb2bbcca35969c3dcc98ab15a473b70a233918
realrgt <ergitorubildo@gmail.com>
2024-06-11 22:42:22 +0200

    add integration tests

---
+++ Bookify.Application.IntegrationTests/Apartments/SearchApartmentsTests.cs
@@ -0,0 +1,40 @@
+﻿using Bookify.Application.Apartments.SearchApartments;
+using FluentAssertions;
+
+namespace Bookify.Application.IntegrationTests;
+
+public class SearchApartmentsTests : BaseIntegrationTest
+{
+    public SearchApartmentsTests(IntegrationTestWebAppFactory factory)
+        : base(factory)
+    {
+    }
+
+    [Fact]
+    public async Task SearchApartments_ShouldReturnEmptyList_WhenDateRangeIsInvalid()
+    {
+        // Arrange
+        var query = new SearchApartmentsQuery(new DateOnly(2024, 1, 10), new DateOnly(2024, 1, 1));
+
+        // Act
+        var result = await Mediator.Send(query);
+
+        // Assert
+        result.IsSuccess.Should().BeTrue();
+        result.Value.Should().BeEmpty();
+    }
+
+    [Fact]
+    public async Task SearchApartments_ShouldReturnApartments_WhenDateRangeIsValid()
+    {
+        // Arrange
+        var query = new SearchApartmentsQuery(new DateOnly(2024, 1, 1), new DateOnly(2024, 1, 10));
+
+        // Act
+        var result = await Mediator.Send(query);
+
+        // Assert
+        result.IsSuccess.Should().BeTrue();
+        result.Value.Should().NotBeEmpty();
+    }
+}
---
+++ Bookify.Application.IntegrationTests/Bookings/GetBookingTests.cs
@@ -0,0 +1,28 @@
+﻿using Bookify.Application.Bookings.GetBooking;
+using Bookify.Domain.Bookings;
+using FluentAssertions;
+
+namespace Bookify.Application.IntegrationTests;
+
+public class GetBookingTests : BaseIntegrationTest
+{
+    private static readonly Guid BookingId = Guid.NewGuid();
+
+    public GetBookingTests(IntegrationTestWebAppFactory factory)
+        : base(factory)
+    {
+    }
+
+    [Fact]
+    public async Task GetBooking_ShouldReturnFailure_WhenBookingIsNotFound()
+    {
+        // Arrange
+        var query = new GetBookingQuery(BookingId);
+
+        // Act
+        var result = await Mediator.Send(query);
+
+        // Assert
+        result.Error.Should().Be(BookingErrors.NotFound);
+    }
+}
